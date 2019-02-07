c571dc50cab769ebde650221171d59b58c2b2613
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2019-02-07 19:12:09 +0200

    Making it compatible with pip

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import uuid
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
-from pip.req import parse_requirements
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -15,10 +12,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
+with open('requirements.txt', 'r') as fd:
+    requirements = list(filter(
+        lambda r: not r.strip().startswith('#'), fd.readlines()))
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-
-requirements = [str(ir.req) for ir in install_reqs]
 test_requirements = requirements
 
 setup(
